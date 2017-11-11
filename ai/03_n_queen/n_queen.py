c63a87702186b33c8e878863237333cecc590706
Georgi Kiryakov <george.kiryakov@gmail.com>
2017-11-11 13:18:14 +0200

    final solution of n queen problem

--- ai/03_n_queen/n_queen.py
+++ ai/03_n_queen/n_queen.py
@@ -3,22 +3,35 @@ import random
 class QueenBoard():
   def __init__(self, queens):
     self.queens_ = queens
-    self.score_ = None
-    self.conflicts_ = {}
     self.size_ = len(self.queens_)
 
     self.left_diagonal = {}
     self.right_diagonal = {}
-    self.row_diagonal = {}
+    self.row = {}
 
-    self.conflict_diagondals_()
+    self.init_conflicts_()
 
-  def conflict_diagondals_(self):
+
+  def put_queen_on_(self, row, col):
+    self.queens_[col] = row
+
+    self.left_diagonal[col+row] = self.left_diagonal.get(col+row, 0) + 1
+    self.right_diagonal[col-row] = self.right_diagonal.get(col-row, 0) + 1
+    self.row[row] = self.row.get(row, 0) + 1
+
+
+  def remove_queen_on_(self, row, col):
+    self.left_diagonal[col+row] = self.left_diagonal[col+row] - 1
+    self.right_diagonal[col-row] = self.right_diagonal[col-row] - 1
+    self.row[row] = self.row[row] - 1
+
+    self.queens_[col] = None
+
+
+  def init_conflicts_(self):
     for c,r in enumerate(self.queens_):
       if r is not None:
-        self.left_diagonal[c+r] = self.left_diagonal.get(c+r, 0) + 1
-        self.right_diagonal[c-r] = self.right_diagonal.get(c-r, 0) + 1
-        self.row_diagonal[r] = self.row_diagonal.get(r, 0) + 1
+        self.put_queen_on_(r, c)
 
 
   def is_queen_on(self, row, col):
@@ -28,34 +41,18 @@ class QueenBoard():
   def move_queen_on(self, queen, position):
     # remove old queen
     if self.queens_[queen] is not None:
-      c, r = queen, self.queens_[queen]
-      self.left_diagonal[c+r] = self.left_diagonal[c+r] - 1
-      self.right_diagonal[c-r] = self.right_diagonal[c-r] - 1
-      self.row_diagonal[r] = self.row_diagonal[r] - 1
+      self.remove_queen_on_(self.queens_[queen], queen)
 
-    # insert new queen
-    c, r = queen, position
-    self.queens_[queen] = position
-
-    self.left_diagonal[c+r] = self.left_diagonal.get(c+r, 0) + 1
-    self.right_diagonal[c-r] = self.right_diagonal.get(c-r, 0) + 1
-    self.row_diagonal[r] = self.row_diagonal.get(r, 0) + 1
+    # put new queen
+    self.put_queen_on_(position, queen)
 
     return self
 
 
   def conflicts(self, row, col):
-    # row_col = "%d_%d" % (row, col)
-    # if row_col in self.conflicts_:
-    #   return self.conflicts_[row_col]
-
     count = 0
-
     # conflicts on row
-    count += self.row_diagonal.get(row, 0)
-
-    # conflicts on col are zero
-    # count += 0
+    count += self.row.get(row, 0)
 
     # conflicts on diagonals
     count += self.left_diagonal.get(col + row, 0)
@@ -64,21 +61,21 @@ class QueenBoard():
     if self.is_queen_on(row, col):
       count -= 3
 
-    # self.conflicts_[row_col] = count
     return count
 
+
   def score(self):
-    # if self.score_ is None:
     conflicts = map(
       lambda c_r: self.conflicts(c_r[1], c_r[0]),
       enumerate(self.queens_)
     )
-    self.score_ = sum(conflicts)
 
-    return self.score_
+    return sum(conflicts)
+
 
   def size(self):
     return self.size_
+
 
   def __str__(self):
     board = ""
@@ -112,18 +109,10 @@ def min_state(board, queen):
   return state
 
 
-def move_queen_on(board, queen, position):
-  next_board = list(board.queens_)
-  next_board[queen] = position
-
-  return QueenBoard(next_board)
-
-
 def max_queen_on_min_state(board):
   m_queen = max_queen(board)
   m_queen_min_state = min_state(board, m_queen)
 
-  # return move_queen_on(board, m_queen, m_queen_min_state)
   return board.move_queen_on(m_queen, m_queen_min_state)
 
 
@@ -131,52 +120,43 @@ def rand_queen_on_rand_state(board):
   rand_queen = random.choice(range(0, board.size()))
   rand_state = random.choice(range(0, board.size()))
 
-  # return move_queen_on(board, rand_queen, rand_state)
   return board.move_queen_on(rand_queen, rand_state)
 
 
 def min_conflicts(start):
   current = start
   last_score = current.score()
-  # ite = 0
   while last_score > 0:
-    # ite +=1
-    # print(last_score)
     current = max_queen_on_min_state(current)
 
     next_score = current.score()
-    if last_score <= next_score:
-      current = rand_queen_on_rand_state(current)
-      next_score = current.score()
+    # escape minimum
+    # if last_score <= next_score:
+    #   current = rand_queen_on_rand_state(current)
+    #   next_score = current.score()
 
     last_score = next_score
-
-  # print(ite, current.score())
   return current
 
 
 def init_board(size):
   board = QueenBoard([None for i in range(size)])
+
   position_to_try = list(range(board.size()))
-  # avg_try = 0
-  chunk = int(board.size() * 0.1)
+  chunk = int(board.size() * 0.1) + 1
+
   for queen in range(board.size()):
     if queen % chunk == 0:
       random.shuffle(position_to_try)
 
-    # n_try = 0
     state = position_to_try[0]
     for r in position_to_try[queen % chunk:]:
-      # n_try += 1
       if board.conflicts(r, queen) < board.conflicts(state, queen):
         state = r
 
       if board.conflicts(state, queen) == 0:
         break
 
-    # avg_try += n_try
-    # print(queen, 'state', state, board.conflicts(state, queen), n_try, avg_try / (queen + 1))
-    # board = move_queen_on(board, queen, state)
     board.move_queen_on(queen, state)
 
   return board
@@ -184,14 +164,8 @@ def init_board(size):
 
 if __name__ == '__main__':
     size = int(input())
-
-    # start_board = QueenBoard([1,2,0,3])
-    # print(start_board.score())
     start_board = init_board(size)
-    # print('ready')
-    # # print(str(start_board))
 
     solution = min_conflicts(start_board)
+    print(solution)
 
-    print(str(solution))
-    # print(solution.queens_)
